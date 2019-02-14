6740f3fe01f04a4332d0aaa20c95025bef442263
Gabriele Lippi <gabriele@lippi.net>
2019-02-14 10:45:06 +0100

    Refactor variable name

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 const prog = require('caporal')
-const createCmd = require('./createTemplate')
+const scaffoldKata= require('./createTemplate')
 
 prog
   .version('1.0.0')
   .command('create', 'Create a new kata')
   .argument('<kata>', 'Name of the kata')
   .option('--t', 'Setup kata with TypeScript', prog.BOOLEAN)
-  .action(createCmd)
+  .action(scaffoldKata)
 
 prog.parse(process.argv)
