e057eee2199ce9864c431c4dea98c36b74eadb6b
Gabriele Lippi <gabriele@lippi.net>
2019-02-15 10:22:43 +0100

    Add test for check directory has copied files

--- test/createTemplate.test.js
+++ test/createTemplate.test.js
@@ -1,20 +1,29 @@
 const shell = require("shelljs");
 
+const KATADIRECTORY = "bowling-kata";
+
 beforeEach(() => {
   // Create a kata named bowling-kata
-  shell.exec("kataclism create bowling-kata");
+  shell.exec(`kataclism create ${KATADIRECTORY}`);
 });
 
 afterEach(() => {
   // Delete the directories created by kataclism
-  shell.rm("-rf", "bowling-kata");
+  shell.rm("-rf", KATADIRECTORY);
 });
 
-test("create a directory with the name of the kata", () => {
+test("Create a directory with the name of the kata", () => {
   const allDirectories = shell.ls();
-  const directoryName = allDirectories.find(
-    fileOrDirectory => fileOrDirectory === "bowling-kata"
-  );
 
-  expect(directoryName).toBe("bowling-kata");
+  expect(allDirectories).toContain(KATADIRECTORY);
+});
+
+test("Copy all files from template/javascript to the new directory just created", () => {
+  const kataDirectory = shell.cd(KATADIRECTORY);
+  const kataDirectoryFiles = shell.ls();
+
+  expect(kataDirectoryFiles).toContain("package.json");
+
+  // go to previous directory in order to remove KATADIRECTORY
+  shell.cd("../");
 });
