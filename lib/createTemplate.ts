870da35eef626bc6fdec7725efb8364f19c0bc44
Gabriele Lippi <gabriele@lippi.net>
2019-02-19 17:19:25 +0100

    Fix templatePath value

--- lib/createTemplate.ts
+++ lib/createTemplate.ts
@@ -4,6 +4,7 @@ const chalk = require("chalk");
 const fs = require('fs-extra')
 const path = require('path')
 import { createPackageJson }  from "./createPackageJson"
+import { templateDirectory } from '../templateDirectory'
 
 function writePackageJson(dir: string, contentJson: any) {
   fs.writeFileSync(path.join(dir, 'package.json'), JSON.stringify(contentJson, null, 2))
@@ -12,11 +13,19 @@ function writePackageJson(dir: string, contentJson: any) {
 export function createTemplate(kata: string, options: any) {
  const templateType = options.t ? "typescript" : "javascript";
  const kataName = kata;
- const localPath = `${process.cwd()}`;
- const templatePath = `${localPath}/templates/${templateType}`;
+ const localPath = process.cwd()
+ const templatePath = `${templateDirectory}/templates/${templateType}`;
  const kataPath = `${localPath}/${kataName}`;
-
  const packageJson = createPackageJson(kataName);
+
+if (fs.existsSync(kataPath)) {
+  console.error(`\n`)
+  console.error(chalk.red(`Can't create kata project wiht name: ${kataName}.`))
+  console.error(`\n`)
+  console.error(chalk.red(`The directory '${kataName}' already exist.`))
+  console.error(`\n`)
+  process.exit(1) 
+}
 
  console.log(chalk.yellow("Scaffolding kata structure..."));
  shell.mkdir("-p", `${kataName}`);
