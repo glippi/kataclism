754814a27524008f0e590033f4f3857eb708d812
Gabriele Lippi <gabriele@lippi.net>
2019-02-20 16:36:54 +0100

    Delete useless stuff

--- build/envDirectoryPath.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEMPLATE_DIRECTORY = __dirname;
--- build/test/envConstants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KATACLISM = "node build/src/index.js";
-exports.KATANAME = "bowling-kata";
-exports.KATADIRECTORY = "bowling-kata";
