f446177d8c5bc98293491c5111446ea12df740b4
Gabriele Lippi <gabriele@lippi.net>
2019-02-20 18:17:57 +0100

    Remove test for index because is useless

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-//import { exec }  from "shelljs";
-//import { KATACLISM, KATANAME } from './envConstants'
-//
-//test("The name provided as argument to the action 'create' of Kataclism should be equal to the value of the key 'kata' returned from the stdout ", () => {
-//  const stdout = exec(`${KATACLISM} create ${KATANAME}`);
-//  console.log({ stdout })
-//  expect(stdout).toMatch(new RegExp(KATANAME));
-//});
