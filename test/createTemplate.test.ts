c0b2d4843def5db7de010821f151e0fde4349a4a
Gabriele Lippi <gabriele@lippi.net>
2019-02-21 18:10:37 +0100

    Remove env constants and small refactor

--- test/createTemplate.test.ts
+++ test/createTemplate.test.ts
@@ -1,55 +1,55 @@
 import { exec, rm, ls, cd, cat } from 'shelljs'
 import { setupVariablesName, createTemplate } from '../src/lib/createTemplate'
-import { KATACLISM, KATANAME, KATADIRECTORY } from './envConstants'
+
+const KATACLISM = 'node lib/index.js'
+const KATA = 'bowling-kata'
 
 beforeEach(() => {
-  exec(`${KATACLISM} create ${KATADIRECTORY}`)
+  exec(`${KATACLISM} create ${KATA}`)
 })
 
 afterEach(() => {
-  rm('-rf', KATADIRECTORY)
+  rm('-rf', KATA)
 })
 
 test('Expect the template to be JavaScript', () => {
-  const { templatePath } = setupVariablesName(KATANAME, {})
+  const { templatePath } = setupVariablesName(KATA, {})
   expect(templatePath).toMatch(new RegExp('javascript'))
 })
 
 test('Expect the template to be TypeScript', () => {
-  const { templatePath } = setupVariablesName(KATANAME, { t: true })
+  const { templatePath } = setupVariablesName(KATA, { t: true })
   expect(templatePath).toMatch(new RegExp('typescript'))
 })
 
 test('Create a directory with the name of the kata', () => {
   const allDirectories = ls()
-  expect(allDirectories).toContain(KATADIRECTORY)
+  expect(allDirectories).toContain(KATA)
 })
 
-test('Abort installation if already exist a directory with the same name as KATADIRECTORY', () => {
-  const kataclismLogErrorInConsole = exec(
-    `${KATACLISM} create ${KATADIRECTORY}`
-  ).stderr
+test('Abort installation if already exist a directory with the same name as KATA', () => {
+  const kataclismLogErrorInConsole = exec(`${KATACLISM} create ${KATA}`).stderr
   expect(kataclismLogErrorInConsole).toBe(
     "\n\nCan't create kata project wiht name: bowling-kata.\n\n\nThe directory 'bowling-kata' already exist.\n\n\n"
   )
 })
 
 test('Copy all files from template/javascript to the new directory just created', () => {
-  const kataDirectoryFiles = ls(KATADIRECTORY)
+  const kataDirectoryFiles = ls(KATA)
   expect(kataDirectoryFiles).toContain('package.json')
 })
 
 test('Install all dependencies', () => {
-  const kataDirectoryFiles = ls(KATADIRECTORY)
+  const kataDirectoryFiles = ls(KATA)
   expect(kataDirectoryFiles).toContain('node_modules')
 })
 
 test("Check if the package.json contain the name of the kata for the 'name' field", () => {
-  const kataPackageJson = cat(`${KATADIRECTORY}/package.json`).stdout
-  expect(kataPackageJson).toMatch(new RegExp(KATANAME))
+  const kataPackageJson = cat(`${KATA}/package.json`).stdout
+  expect(kataPackageJson).toMatch(new RegExp(KATA))
 })
 
 test('Setup README with name provided for the kata', () => {
-  const kataPackageREADME = cat(`${KATADIRECTORY}/README.md`).stdout
-  expect(kataPackageREADME).toMatch(new RegExp(KATANAME))
+  const kataPackageREADME = cat(`${KATA}/README.md`).stdout
+  expect(kataPackageREADME).toMatch(new RegExp(KATA))
 })
--- test/envConstants.ts
+++
@@ -1,3 +0,0 @@
-export const KATACLISM = 'node lib/index.js'
-export const KATANAME = 'bowling-kata'
-export const KATADIRECTORY = 'bowling-kata'
