73f62fdd9dc77b012a0967c61556af596f1fc327
Gabriele Lippi <gabriele@lippi.net>
2019-02-21 15:49:59 +0100

    Rmove useless code

--- test/createTemplate.test.ts
+++ test/createTemplate.test.ts
@@ -11,21 +11,33 @@ afterEach(() => {
   rm("-rf", KATADIRECTORY);
 });
 
+test("Expect the template to be JavaScript", () => {
+  const { templateType } = setupVariablesName(KATANAME, {})
+  expect(templateType).toBe('javascript')
+})
+
+test("Expect the template to be TypeScript", () => {
+  const { templateType } = setupVariablesName(KATANAME, {t:true})
+  expect(templateType).toBe('typescript')
+})
+
 test("Create a directory with the name of the kata", () => {
   const allDirectories = ls();
-
   expect(allDirectories).toContain(KATADIRECTORY);
+});
+
+test("Abort installation if already exist a directory with the same name as KATADIRECTORY", () => {
+  const kataclism = exec(`${KATACLISM} create ${KATADIRECTORY}`);
+  expect(kataclism).toThrowError();
 });
 
 test("Copy all files from template/javascript to the new directory just created", () => {
   const kataDirectoryFiles = ls(KATADIRECTORY);
-
   expect(kataDirectoryFiles).toContain("package.json");
 });
 
-test("Node modules should be already installed inside the new kata folder", () => {
+test("Install all dependencies", () => {
   const kataDirectoryFiles = ls(KATADIRECTORY);
-
   expect(kataDirectoryFiles).toContain("node_modules");
 });
 
@@ -38,13 +50,3 @@ test("Setup README with name provided for the kata", () => {
   const kataPackageREADME = cat(`${KATADIRECTORY}/README.md`).stdout
   expect(kataPackageREADME).toMatch(new RegExp(KATANAME))
 });
-
-test("Expect the template to be JavaScript", () => {
-  const { templateType } = setupVariablesName(KATANAME, {})
-  expect(templateType).toBe('javascript')
-})
-
-test("Expect the template to be TypeScript", () => {
-  const { templateType } = setupVariablesName(KATANAME, {t:true})
-  expect(templateType).toBe('typescript')
-})
