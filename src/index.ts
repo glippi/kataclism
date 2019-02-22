a90982d1508061d29a7f7e58e683623546669da6
Gabriele Lippi <gabriele@lippi.net>
2019-02-22 11:12:05 +0100

    First setup of Inquirer

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 #!/usr/bin/env node
 import { createTemplate } from './lib/createTemplate'
 const cli = require('cac')()
+const inquirer = require('inquirer')
 
-cli
-  .command('create <kata>')
-  .action((kata: string, options: boolean) => {
-    createTemplate(kata, options)
-  })
-  .option('-t, --typescript', 'Setup for TypeScript')
+if (process.argv.length > 2) {
+  cli
+    .command('create <kata>')
+    .action((kata: string, options: boolean) => {
+      createTemplate(kata, options)
+    })
+    .option('-t, --typescript', 'Setup for TypeScript')
 
-cli.parse()
+  cli.parse()
+} else {
+  inquirer
+    .prompt([
+      {
+        type: 'rawlist',
+        name: 'reptile',
+        message: 'Which is better?',
+        choices: ['alligator', 'crocodile'],
+      },
+    ])
+    .then((answers: any) => {
+      console.info('Answer:', answers.reptile)
+    })
+}
