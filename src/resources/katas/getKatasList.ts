d637ef6865ea146f9197cc1546168c11d2545135
Gabriele Lippi <gabriele@lippi.net>
2019-02-28 18:11:55 +0100

    Fix reference of directory for templates still need to create a proper regex

--- src/resources/katas/getKatasList.ts
+++ src/resources/katas/getKatasList.ts
@@ -1,14 +1,20 @@
 import { ls } from 'shelljs'
 
+function exitBuildDirectory(path: string): string {
+  return path.replace('lib/', 'src/')
+}
+
 export function extractFileName(path: string): string {
-  const fileNameWithExtension = path.split('/')[4]
+  // TODO: refactor using regex, can't rely on split and indexing
+  const fileNameWithExtension = path.split('/')[8]
   const fileNameWithoutExtension = fileNameWithExtension.split('.')[0]
   return fileNameWithoutExtension
 }
 
 export function getKatasList() {
   let katas: any = []
-  ls('./src/resources/katas/*.md').forEach((file: any) => {
+  const kataclismDirectory = exitBuildDirectory(__dirname)
+  ls(`${kataclismDirectory}/*.md`).forEach((file: any) => {
     katas.push(file)
   })
   const allKatas = katas.map((file: string) => extractFileName(file))
