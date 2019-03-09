b16cdcd8276c9bcaeffeaf7189ffc0ce0435ef41
Gabriele Lippi <gabriele@lippi.net>
2019-03-09 20:33:53 +0100

    Remove useless log

--- src/lib/chooseKata.ts
+++ src/lib/chooseKata.ts
@@ -2,7 +2,6 @@ import { Answers } from 'inquirer'
 import { createTemplate } from './createTemplate'
 
 export function chooseKata(answers: Answers) {
-  console.log({ answers })
   const { kata, language } = answers
   const formatLanguageOption = language === 'javascript' ? {} : { t: true }
   createTemplate(kata, formatLanguageOption)
