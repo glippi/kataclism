13f3dbf3d4b4e83910b4c14eb769d0409a6808e3
Gabriele Lippi <gabriele@lippi.net>
2019-12-06 00:23:43 +0100

    Delete useless tests and update old one

--- test/chooseKata.test.ts
+++
@@ -1,6 +0,0 @@
-import { chooseKata } from '../src/lib/chooseKata'
-import { exec, rm, ls, cd, cat } from 'shelljs'
-
-test('it should return "bowling-kata"', () => {
-  expect(1).toBe(1)
-})
--- test/getKatasList.test.ts
+++ test/getKatasList.test.ts
@@ -1,20 +1,17 @@
-import { getKatasList } from '../src/lib/getKatasList'
+import { getKatasTitle } from '../src/lib/getKatasList'
 
 it('should verify the list of all available katas', () => {
-  const expectedKataList = getKatasList()
+  const expectedKataList = getKatasTitle()
   const actual = [
     'bank-kata',
     'bowling-kata',
-    'fizz_buzz-kata',
-    'game_of_life-kata',
-    'leap_years-kata',
-    'mars_rover-kata',
-    'roman_numerals-kata',
-    'shopping_basket-kata',
-    'social_networking-kata',
-    'string_calculator-kata',
-    'tennis_refactoring-kata',
-    'tic_tac_toe-kata',
+    'fizz-buzz-kata',
+    'game-of-life-kata',
+    'leap-years-kata',
+    'mars-rover-kata',
+    'roman-numerals-kata',
+    'string-calculator-kata',
+    'tic-tac-toe-kata',
   ]
   expect(expectedKataList).toEqual(actual)
 })
