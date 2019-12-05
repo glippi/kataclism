91b2b4485d71601feead65939b7100b0aa9f128d
Gabriele Lippi <gabriele@lippi.net>
2019-12-06 00:17:12 +0100

    Refactor types for option parameter and replaced old function with new one

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 import { createTemplate } from './lib/createTemplate'
 import { chooseKata } from './lib/chooseKata'
-import { getKatasList } from './lib/getKatasList'
+import { getKatasTitle } from './lib/getKatasList'
 import inquirer from 'inquirer'
 import cac from 'cac'
 
 const cli = cac()
-const allKatas = getKatasList()
+const allKatas = getKatasTitle()
 const isCustomKata = process.argv.length > 2
 
 if (isCustomKata) {
   cli
     .command('create <kata>')
-    .action((kata: string, options: boolean) => {
+    .option('-t, --typescript', 'Setup for TypeScript')
+    .action((kata: string, options: { t?: undefined } | { t: boolean }) => {
       createTemplate(kata, options)
     })
-    .option('-t, --typescript', 'Setup for TypeScript')
 
   cli.parse()
 } else {
