024c4071e7c104fe0d94684c893fddcd1bfc4ab4
Damián Pumar <damianpumar@gmail.com>
2022-03-30 09:27:46 +0200

    🧮 Improve string calculator

--- src/lib/katas/string-calculator.ts
+++ src/lib/katas/string-calculator.ts
@@ -1,6 +1,8 @@
 export const StringCalculator = {
   name: 'string-calculator-kata',
   description: `
+## Instructions
+
 1. Create a simple String calculator with a method int \`Add(string numbers)\`
   * The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
   * Start with the simplest test case of an empty string and move to 1 and two numbers
